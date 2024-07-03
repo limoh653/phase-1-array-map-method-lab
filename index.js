e4ce8c55ce487eef92d01bd75ce57c3fac4d4fdd
limoh653 <limohesbon7@gmail.com>
2024-07-03 22:23:54 +0300

    map()

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => 
+    tutorial
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ')
+  );
+};
+
+const titleCasedTutorials = titleCased();
+console.log(titleCasedTutorials);
